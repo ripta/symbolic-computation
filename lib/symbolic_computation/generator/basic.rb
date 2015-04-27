f11a959c7f45a5cc6ea1f2c7f2ae118349958334
Ripta Pasay <github@r8y.org>
2015-04-26 18:18:03 -0700

    sc/generator: split generator implementation into classes

---
+++ lib/symbolic_computation/generator/basic.rb
@@ -0,0 +1,66 @@
+module SymbolicComputation
+  class Generator
+
+    Basic = Class.new do
+      class <<self
+
+        define_method(:coerce) do |value|
+          coerced_type = coercer_registry.keys.find do |type|
+            # puts "#{value.inspect}.kind_of?(#{type.inspect})"
+            value.kind_of?(type)
+          end
+          if coerced_type.nil?
+            # warn "Cannot coerce #{value.inspect}"
+            value
+          else
+            coercer = coercer_registry[coerced_type]
+            self == coercer ? value : coercer.new(value)
+          end
+        end
+
+        define_method(:coercer_registry) do
+          @@coercer_registry ||= { }
+        end
+
+        define_method(:coerces) do |*target_klasses|
+          coercion_klass = self
+          target_klasses.each do |target_klass|
+            coercion_klass.coercer_registry[target_klass] = coercion_klass
+          end
+
+          # FIXME: this should be defined on the subklass instead, but there are resolution problems
+          # coercion_klass.send(:define_method, :coerce) do |other|
+          Basic.send(:define_method, :coerce) do |other|
+            case other
+            when *target_klasses
+              [coercion_klass.new(other), self]
+            else
+              raise TypeError, "#{self.class.name} cannot be coerced into #{other.class.name}!"
+            end
+          end
+          self
+        end
+
+        define_method(:op) do |op|
+          op_klass = self
+          if Basic.instance_methods.include?(op)
+            raise ArgumentError, "Cannot allow #{op_klass} to operate on #{op.inspect}, because it is already claimed"
+          end
+          Basic.send(:define_method, op) do |*others|
+            #puts "Basic##{op}"
+            # puts "#{self.class} #{op_klass} #{others.first.inspect}"
+            op_klass.new(self, *others)
+          end
+          self
+        end
+
+        define_method(:simplify) do |&blk|
+          @__simplifier__ ||= Simplifier.new
+          blk.nil? ? @__simplifier__ : @__simplifier__.instance_eval(&blk)
+        end
+
+      end
+    end
+
+  end
+end
---
+++ lib/symbolic_computation/generator/class_methods.rb
@@ -0,0 +1,19 @@
+module SymbolicComputation
+  class Generator
+
+    ClassMethods = Module.new do
+
+      # def klass.abstract
+      define_method(:abstract) do
+        self
+      end
+
+      # def klass.implement
+      define_method(:implement) do |&blk|
+        Class.new(self).tap { |k| k.class_eval(&blk) unless blk.nil? }
+      end
+
+    end
+
+  end
+end
