7dfbbcbc03ce0d426db1d40bf93752c3d26394f9
Ripta Pasay <github@r8y.org>
2015-04-26 23:05:24 -0700

    sc/ast: remove unnecessary simplification rule on multiply

--- lib/symbolic_computation/ast/binary_op/multiply.rb
+++ lib/symbolic_computation/ast/binary_op/multiply.rb
@@ -4,10 +4,7 @@ module SymbolicComputation
     class Multiply < BinaryOp.implement
 
       op :*
-      simplify {
-        on_any_order(Numeric, Variable) { |coef, var| Operand.new(Value.new(coef), var) }
-        on_any_order(Value, Variable)   { |coef, var| Operand.new(coef, var) }
-      }
+      simplify.on_any_order(Value, Variable) { |coef, var| Operand.new(coef, var) }
 
     end
 
